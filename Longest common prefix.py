645dc546d22cb212593e23b3ff58a804d8834bcc
iamshubhamkatariya <shubhamkatariya2528@gmail.com>
2026-08-06 10:18:32 +0530

    Longest common prefix.py

--- Longest common prefix.py
+++ Longest common prefix.py
@@ -1,14 +1,12 @@
 def longest_common_prefix(strs):
     if not strs:
+
         return ""
 
-    prefix = strs[0]
+    base = min(strs, key=len)
 
-    for i in range(len(prefix)):
-        char = prefix[i]
-
-        for s in strs[1:]:
-            if i >= len(s) or s[i] != char:
-                return prefix[:i]
-
-    return prefix
+    for i, ch in enumerate(base):
+        for s in strs:
+            if s[i] != ch:
+                return base[:i]
+    return base
