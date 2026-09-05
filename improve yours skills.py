d0998cf9905ed256cfeee6422932457eace5682f
iamshubhamkatariya <shubhamkatariya2528@gmail.com>
2026-09-06 00:01:22 +0530

    palindrome no.

---
+++ improve yours skills.py
@@ -0,0 +1,15 @@
+num = int(input("Enter a number: "))
+
+original = num
+
+rev = 0
+
+while num > 0:
+    digit = num % 10
+    rev = rev * 10 + digit 
+    num = num // 10
+
+    if rev == original:
+        print(f"{original} is a palindrome. ")
+    else:
+        print(f"{original} is not a palindrome. ")
